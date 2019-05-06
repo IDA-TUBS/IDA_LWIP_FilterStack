db00348d8791de30f5f0e3bfb95a03b5230b28b6
Kai-Bjoern Gemlau <gemlau@ida.ing.tu-bs.de>
2019-05-06 11:01:07 +0200

    Moved xilinx driver with modifications to lwip hw port

--- ports/hw/psur5/netif/subdir.mk
+++ ports/hw/psur5/netif/subdir.mk
@@ -7,3 +7,9 @@ SRC += $(LWIP_HW_PSUR5_SRC_DIR)/xadapter.c
 SRC += $(LWIP_HW_PSUR5_SRC_DIR)/xpqueue.c
 SRC += $(LWIP_HW_PSUR5_SRC_DIR)/xemacpsif_ieee1588.c
 SRC += $(LWIP_HW_PSUR5_SRC_DIR)/xemacps_g.c
+
+SRC += $(LWIP_HW_PSUR5_SRC_DIR)/emacps_v3_7/src/xemacps.c
+SRC += $(LWIP_HW_PSUR5_SRC_DIR)/emacps_v3_7/src/xemacps_control.c
+SRC += $(LWIP_HW_PSUR5_SRC_DIR)/emacps_v3_7/src/xemacps_intr.c
+SRC += $(LWIP_HW_PSUR5_SRC_DIR)/emacps_v3_7/src/xemacps_bdring.c
+SRC += $(LWIP_HW_PSUR5_SRC_DIR)/emacps_v3_7/src/xemacps_hw.c
