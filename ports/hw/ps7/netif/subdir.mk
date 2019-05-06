4c17ed09587152d568ddbfd1ce3758e475b92c59
Kai-Bjoern Gemlau <gemlau@ida.ing.tu-bs.de>
2019-05-06 16:04:23 +0200

    Updated file list for ps7

--- ports/hw/ps7/netif/subdir.mk
+++ ports/hw/ps7/netif/subdir.mk
@@ -7,3 +7,9 @@ SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/xadapter.c
 SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/xpqueue.c
 SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/xemacpsif_ieee1588.c
 SRC += $(LWIP_HW_PS7_SRC_DIR)/xemacps_g.c
+
+SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/emacps_v3_7/src/xemacps.c
+SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/emacps_v3_7/src/xemacps_control.c
+SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/emacps_v3_7/src/xemacps_intr.c
+SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/emacps_v3_7/src/xemacps_bdring.c
+SRC += $(LWIP_HW_PS7_SRC_DIR)/../../psur5/netif/emacps_v3_7/src/xemacps_hw.c
